0a82a8524ad5f8e843022144584fdd6b27d1198c
Baptiste Coudray <coudray.baptiste@gmail.com>
2018-06-29 14:27:51 +0200

    Compressed website with gulp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <DNSServer.h>
 #include <ArduinoJson.h>
 #include <FS.h>
-// #include <string.h>
 
 struct Configuration
 {
@@ -26,7 +26,7 @@ int DEFAULT_DNS_PORT = 53;
 int DEFAULT_DNS_TTL = 86400; // 24 hours
 
 Configuration config;
-ESP8266WebServer webServer(DEFAULT_HTTP_PORT);
+AsyncWebServer webServer(DEFAULT_HTTP_PORT);
 DNSServer dnsServer;
 
 void loadConfiguration(const char *filename, Configuration &config)
@@ -49,64 +49,33 @@ void loadConfiguration(const char *filename, Configuration &config)
     file.close();
 }
 
-void serveWebsite()
-{
-    webServer.serveStatic("/", SPIFFS, "/index.html");
-    /* Serve the entire data folder */
-    webServer.serveStatic("/index.html", SPIFFS, "/index.html");
-    webServer.serveStatic("/css/bootstrap.min.css", SPIFFS, "/css/bootstrap.min.css");
-    webServer.serveStatic("/css/dashboard.min.css", SPIFFS, "/css/dashboard.min.css");
-    webServer.serveStatic("/js/bootstrap.bundle.min.js", SPIFFS, "/js/bootstrap.bundle.min.js");
-    webServer.serveStatic("/js/jquery.min.js", SPIFFS, "/js/jquery.min.js");
-    webServer.serveStatic("/js/jailbreak.min.js", SPIFFS, "/js/jailbreak.min.js");
-    webServer.serveStatic("/payloads/ftp.html", SPIFFS, "/payloads/ftp.html");
-    webServer.serveStatic("/payloads/game-dumper.html", SPIFFS, "/payloads/game-dumper.html");
-    webServer.serveStatic("/payloads/kernel-dumper.html", SPIFFS, "/payloads/kernel-dumper.html");
-    webServer.serveStatic("/payloads/hen.html", SPIFFS, "/payloads/hen.html");
-    webServer.serveStatic("/payloads/enable-vr.html", SPIFFS, "/payloads/enable-vr.html");
-    webServer.serveStatic("/payloads/db-sg-backup.html", SPIFFS, "/payloads/db-sg-backup.html");
-    webServer.serveStatic("/payloads/app-to-usb.html", SPIFFS, "/payloads/app-to-usb.html");
-
-    /* Some how this is not working
-    Dir dir = SPIFFS.openDir("/");
-    while (dir.next())
-    {
-        const char *filename = dir.fileName().c_str();
-        // Exclude configuration file
-        if (!strcmp(DEFAULT_CONFIG_FILENAME, filename)
-            continue;
-        webServer.serveStatic(filename, SPIFFS, filename);
-    } */
-}
-
 void setup()
 {
     delay(1000);
     Serial.begin(9600);
-    
+
     /* Loading general configuration */
     if (!SPIFFS.begin())
         Serial.println("Failed to mount SPIFFS");
     loadConfiguration(DEFAULT_CONFIG_FILENAME, config);
-    
+
     /* Settings up Wi-Fi AP */
     WiFi.softAPConfig(config.ip_address, config.gateway, config.subnet);
     WiFi.softAP(config.ssid, config.password);
-    
+
     /* Settings up DNS Server */
     dnsServer.setTTL(DEFAULT_DNS_TTL);
     dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
-    
-     /* Redirect all domains to the local WebServer */
+
+    /* Redirect all domains to the local WebServer */
     dnsServer.start(DEFAULT_DNS_PORT, "*", config.ip_address);
-    
+
     /* Settings up WebServer */
-    serveWebsite();
+    webServer.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
     webServer.begin();
 }
 
 void loop()
 {
     dnsServer.processNextRequest();
-    webServer.handleClient();
 }
